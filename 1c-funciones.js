67f39f6ee0863ca805a3795430a0f20fda92c1ca
nicolas-CaC <nicolas.machicado@bue.edu.ar>
2022-10-05 10:08:07 -0300

    Comentarios

--- 1c-funciones.js
+++ 1c-funciones.js
@@ -1,13 +1,16 @@
 console.clear();
 
+// FUNCION CLASICA
 function funcion() {
     console.log('Hola mamá')
 }
 
+// FUNCION ANóNIMA
 const funcion2 = function () {
     console.log('Hola papu')
 }
 
+// FUNCION FLECHA (Expresión Lambda)
 const funcion3 = () => {
     console.log('Hola Bro')
 }
